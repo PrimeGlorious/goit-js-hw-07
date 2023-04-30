685a2444994b99d77bc9c2733c0fa51b23775255
PrimeGlorious <TheComboPunk@gmail.com>
2023-04-30 19:20:46 +0300

    finished

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryItemsMarkup = makeGalleryItemMarkup(galleryItems);
+populateGallery(galleryItemsMarkup);
+
+galleryRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) { return };
+
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  window.addEventListener('keydown', onModalClose);
+
+  function onModalClose(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+    };
+  };
+};
+
+function makeGalleryItemMarkup(items) {
+  return items.map(({preview, original, description}) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+  `).join('');
+};
+
+function populateGallery(markup) {
+  galleryRef.innerHTML = markup;
+};
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const galleryItemsMarkup = makeGalleryItemMarkup(galleryItems);
+populateGallery(galleryItemsMarkup);
+
+function makeGalleryItemMarkup(items) {
+  return items.map(({preview, original, description}) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>
+  `).join('');
+};
+
+function populateGallery(markup) {
+  galleryRef.innerHTML = markup;
+};
+
+const lightbox = new SimpleLightbox('.gallery__link', { captionSelector: 'img', captionType: 'attr', captionDelay: 250, captionPosition: 'bottom', captionsData: 'alt', });
